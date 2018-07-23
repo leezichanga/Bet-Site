9dccc14e44d13fcf078bc8f50cc9dbea4a1dcafb
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-07-23 19:12:29 +0300

    change timezone to africa/Nairobi

--- bet/settings.py
+++ bet/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'betsite',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
