bf0b2f48428cf7e7c0b31ba4eefa10668fdf646a
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-07-24 23:02:24 +0300

    set urls for forms

--- betsite/urls.py
+++ betsite/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
 url('$',views.home,name='home'),
+url(r'^deposit/',views.deposit, name='deposit'),
+url(r'^placebet/',views.placebet, name='placebet')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
