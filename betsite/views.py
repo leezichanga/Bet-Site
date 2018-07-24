c0efe49b44d5e5d0c41b3b4ec9542083f2e1405d
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-07-24 20:45:39 +0300

    add deposit model

--- betsite/views.py
+++ betsite/views.py
@@ -1,10 +1,79 @@
-from django.shortcuts import render,redirect, get_object_or_404
-from django.http import HttpResponse
-# from django.template import RequestContext
-# from django.contrib.auth import authenticate,login
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,
+import logging
 
-# Create your views here.
-# @login_required(login_url='/accounts/login')
+# Create your views Here
+logging=logging.getlogger('auth')
+# register user
+from django.views import views
+from authentication.models import User
+from django.core.mail import EmailMessage
+
+class UserRegistration(View):
+    def post(self,reqest,*args,**kwargs):
+        try:
+            pass
+            # get form data
+            email=request.POST['email']
+            logging.info(email)
+            password=request.POST['password']
+            logging.info(password)
+            #save user data
+            user=User(
+            email=email,
+            password=password
+            )
+            user.save()
+            # get user object
+            user=User.objects.get(email=email)
+            email_subject='ACTIVATE ACCOUNT'
+            email_body="""
+            Thank you for registering at Safari betting company,
+            Please activate your account here http://127.0.0.1:8000/?quetip={0}
+            """.format(str(user.id))
+            email = EmailMessage('ACTIVATE ACCOUNT', 'body', to=[email])
+            email.send()
+            #  send otp
+        except Exception as e:
+            raise e
+
+class EmailValidation(View):
+    def get (self,request,*args,**kwargs):
+        try:
+            '''
+            http://liz.co.ke/validate?id=user.id
+            Users.objects.get(ser_id-id).is_active=True.save(
+            )
+            '''
+        except Exception as e:
+            raise
+        else:
+            pass
+        finally:
+            pass
+
+
+class Placebet(self,request,*args,**kwargs):
+    try:
+        # form data
+
+        game_id=request.POST['game_id']
+        amount=request.POST['amount']
+        prediction=request.POST['prediction']
+
+        # save user data
+        user=User(
+        game_id=game_id
+        amuont=amount
+        prediction=prediction
+        )
+         send email bet placed
+         '''
+     except Exception as e:
+         raise
+     else:
+         pass
+     finally:
+         pass
+
 def home(request):
     return render(request, 'index.html')
